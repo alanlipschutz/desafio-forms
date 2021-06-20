551706caa52f98a0b4640c07caf622a92aa0ee85
Alan Lipschutz <alanelipschutz@gmail.com>
2021-06-20 11:48:50 -0300

    commit con todo hecho

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function main() {}
+function main() {
+  const myFormEl = document.querySelector(".my-form");
+  myFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+    value.temas = data.getAll("temas");
+    console.log({ value });
+  });
+}
 
 main();
